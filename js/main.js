c51d4c2bef129953aa9535887b36e72d3627941c
dj-algorhythm <berryman.joseph4228@gmail.com>
2014-12-09 18:59:39 -0600

    Catching up

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
+var setImgSize = function (){
+    /*var imgWindowWidth = $('.img-window').width();
+    $('.img-window > img').width(.7816128773924881877152238327861 * imgWindowWidth);
+    var imgWidth = .7816128773924881877152238327861 * imgWindowWidth;
+    $('.img-window > img').height(imgWidth * 1/1.5121951219512195121951219512195);*/
+    var winWidth = $('.img-window').width();
+    $('.gall-image').height(winWidth * 1/1.333333);
+    $('.gall-image').width('100%');
+};
+
 $(document).ready(function(){
   $(".hisrc img").hisrc();
   $(".hisrc img+img").hisrc({
@@ -5,30 +15,33 @@ $(document).ready(function(){
     speedTestUri: '50K.jpg'
   });
 
-  $(".backstretch-carousel").backstretch("./img/bg-gallery2.png");
-  //var galRows = document.getElementsByClassName("gallery-row");
-  //var colsRow1 = galRows[0].childNodes;
-  //var colsRow2 = galRows[1].childNodes;
+  $(".backstretch-carousel").backstretch([
+    "./img/bg-services.jpg",
+    "./img/bg-gallery1.jpg",
+    "./img/bg-gallery2.jpg"],
+    {duration:1000,fade:1000});
 
-  var setImgSize = function (){
-      /*var imgWindowWidth = $('.img-window').width();
-      $('.img-window > img').width(.7816128773924881877152238327861 * imgWindowWidth);
-      var imgWidth = .7816128773924881877152238327861 * imgWindowWidth;
-      $('.img-window > img').height(imgWidth * 1/1.5121951219512195121951219512195);*/
-      var winWidth = $('.img-window').width();
-      $('.gall-image').height(winWidth * 1/1.333333);
-      $('.gall-image').width('100%');
-  };
+  $(".backstretch-carousel").backstretch("pause");
+
+  $('.screen').waypoint(function(direction) {
+    if (direction === 'down') {
+     $(".backstretch-carousel").backstretch("next");
+    }
+    if (direction === 'up') {
+     $(".backstretch-carousel").backstretch("prev");
+    }
+  }, { offset: '50%' });
+
 
   setImgSize();
 
   $('.gall-image').hover(function(event){
       var imgSrc = $(event.currentTarget).attr("src");
-      var newSrc = imgSrc.replace(".png", "Tint.png");
+      var newSrc = imgSrc.replace(".jpg", "Tint.jpg");
       $(event.currentTarget).attr("src", newSrc);
   },function(){
       var imgSrc = $(event.currentTarget).attr("src");
-      var newSrc = imgSrc.replace("Tint.png", ".png");
+      var newSrc = imgSrc.replace("Tint.jpg", ".jpg");
       $(event.currentTarget).attr("src", newSrc);
   });
   //$(document).resize(setImgSize());
